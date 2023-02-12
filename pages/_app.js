640a468a6e806a1efbfcc7368a3197270def403d
Kobs <ckasergio@gmail.com>
2023-02-13 01:16:37 +0800

    google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
+import Script from "next/script";
 import "../styles/globals.css";
 import "../styles/hero.css";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer', '${process.env.GOOGLE_ANALYTICS}');
+    `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
