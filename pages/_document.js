8ec11e26d7dc055c5d98b634e78c073cd1bd69ad
imn0tmich <mamichellepantoja@gmail.com>
2023-07-04 10:11:59 +0800

    vercel push, merged from aa

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
+    
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
